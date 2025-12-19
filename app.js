5c27e41a02f026c3f29173bbb11edffabcc2fe3e
GUSTAMARIANO <luizguga235711@gmail.com>
2025-12-19 16:01:50 -0300

    criação do arquivo css e javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+const tecla = document.querySelector('.btn');
+const visor = document.querySelector('#visor');
+const teste = document.querySelector('#t6');
+
+tecla.addEventListener('click', function(){
+    console.log('teste');
+    visor.textContent = "Dark";
+    
+});
